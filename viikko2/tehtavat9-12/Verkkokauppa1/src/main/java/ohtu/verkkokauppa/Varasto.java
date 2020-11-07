79d6827f5e9e600d48fc90ebc7bb60dbe449b7c4
xlexle <xlexle@cs.helsinki.fi>
2020-11-07 20:19:47 +0200

    viikko2 Kirjanpito-luokan käyttö rajapintojen kautta, singletonien poisto

--- viikko2/tehtavat9-12/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Varasto.java
+++ viikko2/tehtavat9-12/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Varasto.java
@@ -4,27 +4,17 @@ import java.util.*;
 
 public class Varasto implements VarastoInterface {
 
-    private static Varasto instanssi;
-
-    public static Varasto getInstance() {
-        if (instanssi == null) {
-            instanssi = new Varasto();
-        }
-
-        return instanssi;
-    }
-
-    private Kirjanpito kirjanpito;
+    private KirjanpitoInterface kirjanpito;
     private HashMap<Tuote, Integer> saldot;
 
-    private Varasto() {
-        kirjanpito = Kirjanpito.getInstance();
+    public Varasto(KirjanpitoInterface kirjanpito) {
+        this.kirjanpito = kirjanpito;
         saldot = new HashMap<Tuote, Integer>();
         alustaTuotteet();
     }
 
     @Override
-    public Tuote haeTuote(int id){
+    public Tuote haeTuote(int id) {
         for (Tuote t : saldot.keySet()) {
             if ( t.getId()==id) return t;
         }
@@ -33,18 +23,18 @@ public class Varasto implements VarastoInterface {
     }
 
     @Override
-    public int saldo(int id){
+    public int saldo(int id) {
         return saldot.get(haeTuote(id));
     }
 
     @Override
-    public void otaVarastosta(Tuote t){
+    public void otaVarastosta(Tuote t) {
         saldot.put(t,  saldo(t.getId())-1 );
         kirjanpito.lisaaTapahtuma("otettiin varastosta "+t);
     }
 
     @Override
-    public void palautaVarastoon(Tuote t){
+    public void palautaVarastoon(Tuote t) {
         saldot.put(t,  saldo(t.getId())+1 );
         kirjanpito.lisaaTapahtuma("palautettiin varastoon "+t);
     }
--- viikko2/tehtavat9-12/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Viitegeneraattori.java
+++ viikko2/tehtavat9-12/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Viitegeneraattori.java
@@ -2,24 +2,14 @@ package ohtu.verkkokauppa;
 
 public class Viitegeneraattori implements ViitegeneraattoriInterface {
 
-    private static Viitegeneraattori instanssi;
-
-    public static Viitegeneraattori getInstance() {
-        if (instanssi == null) {
-            instanssi = new Viitegeneraattori();
-        }
-
-        return instanssi;
-    }
-
     private int seuraava;
 
-    private Viitegeneraattori(){
+    public Viitegeneraattori() {
         seuraava = 1;
     }
 
     @Override
-    public int uusi(){
+    public int uusi() {
         return seuraava++;
     }
 }
