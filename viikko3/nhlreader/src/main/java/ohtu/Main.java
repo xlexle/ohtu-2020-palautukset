b4283d1e497683b42d06f6a73be0921519154711
xlexle <xlexle@cs.helsinki.fi>
2020-11-11 21:42:54 +0200

    viikko3 printtaa suomalaiset pelaajat

--- viikko3/nhlreader/src/main/java/ohtu/Main.java
+++ viikko3/nhlreader/src/main/java/ohtu/Main.java
@@ -2,24 +2,24 @@ package ohtu;
 
 import com.google.gson.Gson;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Date;
+
 import org.apache.http.client.fluent.Request;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         String url = "https://nhlstatisticsforohtu.herokuapp.com/players";
-
         String bodyText = Request.Get(url).execute().returnContent().asString();
-
-        System.out.println("json-muotoinen data:");
-        System.out.println( bodyText );
-
         Gson mapper = new Gson();
         Player[] players = mapper.fromJson(bodyText, Player[].class);
 
-        System.out.println("Oliot:");
-        for (Player player : players) {
-            System.out.println(player);
-        }
+        System.out.println("Players from FIN" + new Date());
+        System.out.println("\nname, team, goals, assists");
+
+        Arrays.stream(players)
+            .filter(it -> it.getNationality().equals("FIN"))
+            .forEach(System.out::println);
     }
 
 }
--- viikko3/nhlreader/src/main/java/ohtu/Player.java
+++ viikko3/nhlreader/src/main/java/ohtu/Player.java
@@ -3,6 +3,10 @@ package ohtu;
 
 public class Player {
     private String name;
+    private String nationality;
+    private int assists;
+    private int goals;
+    private String team;
 
     public void setName(String name) {
         this.name = name;
@@ -12,9 +16,42 @@ public class Player {
         return name;
     }
 
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
+
+    public int getAssists() {
+        return assists;
+    }
+
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
+
+    public int getGoals() {
+        return goals;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
     @Override
     public String toString() {
-        return name;
+        String[] player = new String[]{name, team, String.valueOf(goals), String.valueOf(assists)};
+        return String.join(", ", player);
     }
       
 }
