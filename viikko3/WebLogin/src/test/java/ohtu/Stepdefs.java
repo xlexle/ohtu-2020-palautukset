38458b9844f96c7ad7a163fc5da647551793306d
xlexle <xlexle@cs.helsinki.fi>
2020-11-15 13:44:31 +0200

    viikko3 nonexistent user can not login testi + whitespace korjauksia

--- viikko3/WebLogin/src/test/java/ohtu/Stepdefs.java
+++ viikko3/WebLogin/src/test/java/ohtu/Stepdefs.java
@@ -15,56 +15,61 @@ public class Stepdefs {
     //WebDriver driver = new ChromeDriver();
     WebDriver driver = new HtmlUnitDriver();
     String baseUrl = "http://localhost:4567";
-    
+
     @Given("login is selected")
     public void loginIsSelected() {
         driver.get(baseUrl);
-        WebElement element = driver.findElement(By.linkText("login"));       
-        element.click();   
-    }    
-    
+        WebElement element = driver.findElement(By.linkText("login"));
+        element.click();
+    }
+
     @When("correct username {string} and password {string} are given")
     public void correctUsernameAndPasswordAreGiven(String username, String password) {
         logInWith(username, password);
-    }    
-    
+    }
+
     @Then("user is logged in")
     public void userIsLoggedIn() {
         pageHasContent("Ohtu Application main page");
-    }    
- 
+    }
+
     @When("correct username {string} and incorrect password {string} are given")
     public void correctUsernameAndIncorrectPasswordAreGiven(String username, String password) {
         logInWith(username, password);
-    }    
-    
+    }
+
+    @When("nonexistent username {string} and some password {string} are given")
+    public void nonexistentUsernameAndSomePasswordAreGiven(String username, String password) {
+        logInWith(username, password);
+    }
+
     @Then("user is not logged in and error message is given")
     public void userIsNotLoggedInAndErrorMessageIsGiven() {
         pageHasContent("invalid username or password");
         pageHasContent("Give your credentials to login");
-    }    
-    
+    }
+
     @When("username {string} and password {string} are given")
     public void usernameAndPasswordAreGiven(String username, String password) throws Throwable {
         logInWith(username, password);
-    }   
-    
+    }
+
     @Then("system will respond {string}")
     public void systemWillRespond(String pageContent) throws Throwable {
         assertTrue(driver.getPageSource().contains(pageContent));
     }
-    
+
     @After
     public void tearDown(){
         driver.quit();
     }
-        
+
     /* helper methods */
- 
+
     private void pageHasContent(String content) {
         assertTrue(driver.getPageSource().contains(content));
     }
-        
+
     private void logInWith(String username, String password) {
         assertTrue(driver.getPageSource().contains("Give your credentials to login"));
         WebElement element = driver.findElement(By.name("username"));
@@ -72,6 +77,6 @@ public class Stepdefs {
         element = driver.findElement(By.name("password"));
         element.sendKeys(password);
         element = driver.findElement(By.name("login"));
-        element.submit();  
-    } 
+        element.submit();
+    }
 }
--- viikko3/WebLogin/src/test/resources/ohtu/logging_in.feature
+++ viikko3/WebLogin/src/test/resources/ohtu/logging_in.feature
@@ -10,3 +10,7 @@ Feature: As a registered user can log in with valid username/password-combinatio
         When correct username "jukka" and incorrect password "wrong" are given
         Then user is not logged in and error message is given
 
+    Scenario: nonexistent user can not login
+        Given login is selected
+        When nonexistent username "whodat" and some password "passu" are given
+        Then user is not logged in and error message is given
