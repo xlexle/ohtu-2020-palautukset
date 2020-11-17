2afffefa2664ce42381ec2dcbaf4a5131dd1212e
xlexle <xlexle@cs.helsinki.fi>
2020-11-17 19:27:53 +0200

    viikko4 ostaLounas-metodin korjaus

--- viikko4/MaksukorttiMockito/src/main/java/ohtu/matkakortti/Kassapaate.java
+++ viikko4/MaksukorttiMockito/src/main/java/ohtu/matkakortti/Kassapaate.java
@@ -7,12 +7,17 @@ public class Kassapaate {
     public Kassapaate() {
         this.myytyjaLounaita = 0;
     }
-    
+
     public void lataa(Maksukortti kortti, int summa){
         kortti.lataa(summa);
     }
-    
+
     public void ostaLounas(Maksukortti kortti) {
+        if (kortti.getSaldo() < HINTA) {
+            System.out.println("Kortilla ei ole saldoa.");
+            return; // TODO throw custom exception instead of printing and returning
+        }
+
         kortti.osta(HINTA);
         myytyjaLounaita++;
     }
