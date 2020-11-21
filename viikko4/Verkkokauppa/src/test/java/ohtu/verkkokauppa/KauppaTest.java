10814e73a4bf5870956faaa7f00d35d4ab9dc30f
xlexle <xlexle@cs.helsinki.fi>
2020-11-21 17:52:58 +0200

    viikko4 muokkaa esimerkkitestiä: varmista tilisiirto-metodin parametrit

--- viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -6,31 +6,35 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class KauppaTest {
+    private int viitenro = 42;
+    private String nimi = "pekka";
+    private String tilinumero = "12345";
+    private Tuote maito = new Tuote(1, "maito", 5);
+    private String kaupanTili = "33333-44455"; // kovakoodattu Kauppa-luokassa
 
     @Test
-    public void ostoksenPaaytyttyaPankinMetodiaTilisiirtoKutsutaan() {
-        // luodaan ensin mock-oliot
+    public void yhdenTuotteenOstoksenPaaytyttyaPankinMetodiaTilisiirtoKutsutaanOikein() {
         Pankki pankki = mock(Pankki.class);
 
         Viitegeneraattori viite = mock(Viitegeneraattori.class);
-        // määritellään että viitegeneraattori palauttaa viitten 42
-        when(viite.uusi()).thenReturn(42);
+        when(viite.uusi()).thenReturn(viitenro);
 
         Varasto varasto = mock(Varasto.class);
-        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
         when(varasto.saldo(1)).thenReturn(10);
-        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        when(varasto.haeTuote(1)).thenReturn(maito);
 
-        // sitten testattava kauppa
         Kauppa k = new Kauppa(varasto, pankki, viite);
-
-        // tehdään ostokset
         k.aloitaAsiointi();
-        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
-        k.tilimaksu("pekka", "12345");
+        k.lisaaKoriin(1); // maito id
+        k.tilimaksu(nimi, tilinumero);
 
-        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
-        verify(pankki).tilisiirto(anyString(), anyInt(), anyString(), anyString(),anyInt());
-        // toistaiseksi ei välitetty kutsussa käytetyistä parametreista
+        verify(pankki).tilisiirto(
+            eq(nimi),
+            eq(viitenro),
+            eq(tilinumero),
+            eq(kaupanTili),
+            eq(maito.getHinta())
+        );
     }
+
 }
