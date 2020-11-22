ad28af02e504d98533d8cc87a6a5ec194eaf6d80
xlexle <xlexle@cs.helsinki.fi>
2020-11-22 12:59:35 +0200

    viikko4 TennisGame-luokan metodien refaktorointi

--- viikko4/Tennis/src/main/java/ohtu/TennisGame.java
+++ viikko4/Tennis/src/main/java/ohtu/TennisGame.java
@@ -1,11 +1,28 @@
 package ohtu;
 
+import java.util.Map;
+
 public class TennisGame {
 
     private int player1Score = 0;
     private int player2Score = 0;
     private String player1Name;
     private String player2Name;
+
+    private Map<Integer, String> scoreStrings = Map.of(
+        0, "Love",
+        1, "Fifteen",
+        2, "Thirty",
+        3, "Forty"
+    );
+
+    private Map<Integer, String> tiedScoreStrings = Map.of(
+        0, "Love-All",
+        1, "Fifteen-All",
+        2, "Thirty-All",
+        3, "Forty-All",
+        4, "Deuce"
+    );
 
     public TennisGame(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -21,61 +38,26 @@ public class TennisGame {
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (player1Score==player2Score)
-        {
-            switch (player1Score)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                case 3:
-                        score = "Forty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
+        if (player1Score == player2Score) {
+            return tiedScoreStrings.get(player1Score);
+        }
 
-            }
+        if (player1Score >= 4 || player2Score >= 4) {
+            return advantageOrWin();
         }
-        else if (player1Score>=4 || player2Score>=4)
-        {
-            int minusResult = player1Score-player2Score;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = player1Score;
-                else { score+="-"; tempScore = player2Score;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
-        }
-        return score;
+
+        return scoreStrings.get(player1Score) + "-" + scoreStrings.get(player2Score);
     }
+
+    private String advantageOrWin() {
+        int difference = Math.abs(player1Score - player2Score);
+        String leader = player1Score > player2Score ? player1Name : player2Name;
+
+        if (difference == 1) {
+            return "Advantage " + leader;
+        }
+
+        return "Win for " + leader;
+    }
+
 }
