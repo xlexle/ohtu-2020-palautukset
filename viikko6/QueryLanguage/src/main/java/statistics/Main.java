857b8f07af96f0b9c0294571ac13de58f0af793b
xlexle <xlexle@cs.helsinki.fi>
2020-12-06 17:56:24 +0200

    viikko6 QueryBuilder any toteutus

--- viikko6/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -9,9 +9,14 @@ public class Main {
         Statistics stats = new Statistics(new PlayerReaderImpl(url));
 
         QueryBuilder query = new QueryBuilder();
-        Matcher m = query.playsIn("NYR")
-            .hasAtLeast(5, "goals")
-            .hasFewerThan(10, "goals").build();
+        Matcher m = query.any(
+            query.playsIn("PHI")
+                .hasAtLeast(10, "assists")
+                .hasFewerThan(5, "goals").build(),
+
+            query.playsIn("EDM")
+                .hasAtLeast(40, "points").build()
+        ).build();
 
         for (Player player : stats.matches(m)) {
             System.out.println(player);
--- viikko6/QueryLanguage/src/main/java/statistics/QueryBuilder.java
+++ viikko6/QueryLanguage/src/main/java/statistics/QueryBuilder.java
@@ -1,35 +1,46 @@
 package statistics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import statistics.matcher.All;
 import statistics.matcher.And;
 import statistics.matcher.HasAtLeast;
 import statistics.matcher.HasFewerThan;
 import statistics.matcher.Matcher;
+import statistics.matcher.Or;
 import statistics.matcher.PlaysIn;
 
 public class QueryBuilder {
-    private Matcher matcher;
+    private List<Matcher> matchers = new ArrayList<Matcher>();
 
     public QueryBuilder() {
-        matcher = new All();
+        matchers.add(new All());
     }
 
     public QueryBuilder hasAtLeast(int value, String category) {
-        matcher = new And(matcher, new HasAtLeast(value, category));
+        matchers.add(new HasAtLeast(value, category));
         return this;
     }
 
     public QueryBuilder hasFewerThan(int value, String category) {
-        matcher = new And(matcher, new HasFewerThan(value, category));
+        matchers.add(new HasFewerThan(value, category));
         return this;
     }
 
     public QueryBuilder playsIn(String team) {
-        matcher = new And(matcher, new PlaysIn(team));
+        matchers.add(new PlaysIn(team));
+        return this;
+    }
+
+    public QueryBuilder any(Matcher... alternatives) {
+        matchers.add(new Or(alternatives));
         return this;
     }
 
     public Matcher build() {
-        return matcher;
+        Matcher every = new And(matchers.toArray(new Matcher[0]));
+        matchers.clear();
+        return every;
     }
 }
